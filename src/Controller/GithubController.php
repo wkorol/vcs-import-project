2ac01f7ede19bd3285c0d41108b641bb3d0f15cf
wkorol <wiktorkorol@icloud.com>
2022-01-20 14:21:22 +0100

    Token test

--- src/Controller/GithubController.php
+++ src/Controller/GithubController.php
@@ -32,7 +32,8 @@ class GithubController extends AbstractController
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json', 'User-Agent: vcs-import-project', 'Authorization: token ' . $this->getParameter('githubtoken')));
+        $token = $this->getParameter('githubtoken');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json', 'User-Agent: vcs-import-project', 'Authorization: token ' . $token ));
         $rep = json_decode(curl_exec($ch), true);
         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         if ($httpcode == '404') {
