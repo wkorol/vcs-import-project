6dccbd79d16d1d6263a6826859533cd21948c512
wkorol <wiktorkorol@gmail.com>
2022-03-03 11:30:53 +0100

    Messenger updates

--- src/MessageHandler/BitbucketImportCommandHandler.php
+++ src/MessageHandler/BitbucketImportCommandHandler.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 namespace App\MessageHandler;
 use App\Message\ImportCommandCreator;
+use App\Services\BitbucketService;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class BitbucketImportCommandHandler implements MessageHandlerInterface {
-    public function __invoke() {
+
+    public function __invoke(ImportCommandCreator $message) {
 
     }
 }
 
-
-?>
--- src/MessageHandler/GithubImportCommandHandler.php
+++ src/MessageHandler/GithubImportCommandHandler.php
@@ -4,19 +4,16 @@ declare(strict_types=1);
 namespace App\MessageHandler;
 
 use App\Message\ImportCommandCreator;
+use App\Services\GithubService;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class GithubImportCommandHandler implements MessageHandlerInterface {
 
-    public function __construct() {
 
-    }
-    public function __invoke() {
-        //test//
+    public function __invoke(ImportCommandCreator $message) {
+
     }
 
 }
 
 
-
-?>
